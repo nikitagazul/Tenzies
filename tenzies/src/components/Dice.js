3acad19ac69e86ceb9078e478883fc0b413bcbaf
Mykyta Gazul <nikitagazul@gmail.com>
2022-11-24 16:55:47 -0500

    ternary styling added

--- tenzies/src/components/Dice.js
+++ tenzies/src/components/Dice.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 export default function Dice(props){
+    const styles ={
+        backgroundColor: props.isHeld ? "#59E391" : "fff"
+    }
     return(
-        <div className="dice">
+        <div className="dice" style={styles}>
             <h2 className="dice--style">{props.die}</h2>
         </div>
     )
